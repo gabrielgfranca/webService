925c231549205c6ebc144365b3a78bd9fda8c973
Gabriel França <gabrielgfranca7@proton.me>
2026-08-24 16:58:35 -0300

    Initial Commit

---
+++ src/test/java/com/gabriel/webService/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.webService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
