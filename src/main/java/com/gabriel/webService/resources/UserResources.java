aad0cd6c14f47e215c7d1c47d23a540f5675eb3c
Gabriel França <gabrielgfranca7@proton.me>
2026-08-26 17:58:27 -0300

    feat: Service layer, component registration

--- src/main/java/com/gabriel/webService/resources/UserResources.java
+++ src/main/java/com/gabriel/webService/resources/UserResources.java
@@ -1,18 +1,32 @@
 package com.gabriel.webService.resources;
 
 import com.gabriel.webService.entities.User;
+import com.gabriel.webService.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResources {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User testUser = new User(1L, "Gabriel", "gabriel@example.com", "123456789", "987654321");
-        return ResponseEntity.ok().body(testUser);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/gabriel/webService/services/UserService.java
@@ -0,0 +1,23 @@
+package com.gabriel.webService.services;
+
+import com.gabriel.webService.entities.User;
+import com.gabriel.webService.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
