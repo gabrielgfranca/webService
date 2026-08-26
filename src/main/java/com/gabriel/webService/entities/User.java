1cc2c2ebb9f8bff9ef1c496409d9bc4abf83164b
Gabriel França <gabrielgfranca7@proton.me>
2026-08-26 18:11:37 -0300

    feat: Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/gabriel/webService/entities/User.java
+++ src/main/java/com/gabriel/webService/entities/User.java
@@ -3,6 +3,8 @@ package com.gabriel.webService.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,6 +20,9 @@ public class User implements Serializable {
     private String email;
     private String password;
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -68,6 +73,10 @@ public class User implements Serializable {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
