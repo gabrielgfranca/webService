a94a31cd90406f266e773aead2107ab531a42a01
Gabriel França <gabrielgfranca7@proton.me>
2026-09-02 19:25:01 -0300

    feat: Products

--- src/main/java/com/gabriel/webService/config/TestConfig.java
+++ src/main/java/com/gabriel/webService/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.gabriel.webService.config;
 
 import com.gabriel.webService.entities.Category;
 import com.gabriel.webService.entities.Order;
+import com.gabriel.webService.entities.Product;
 import com.gabriel.webService.entities.User;
 import com.gabriel.webService.entities.enums.OrderStatus;
 import com.gabriel.webService.repositories.CategoryRepository;
 import com.gabriel.webService.repositories.OrderRepository;
+import com.gabriel.webService.repositories.ProductRepository;
 import com.gabriel.webService.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,9 +30,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         Category c1 = new Category(null, "Electronics");
         Category c2 = new Category(null, "Books");
         Category c3 = new Category(null, "Clothing");
@@ -45,5 +56,6 @@ public class TestConfig implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     }
 }
---
+++ src/main/java/com/gabriel/webService/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.gabriel.webService.services;
+
+import com.gabriel.webService.entities.Product;
+import com.gabriel.webService.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
