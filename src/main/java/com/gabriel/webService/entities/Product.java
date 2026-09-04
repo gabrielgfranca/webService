afb3e9a18517673d3aae32cd6224005555f5ae94
Gabriel França <gabrielgfranca7@proton.me>
2026-09-04 15:06:26 -0300

    feat: Product-OrderItem one-to-many association

--- src/main/java/com/gabriel/webService/entities/Product.java
+++ src/main/java/com/gabriel/webService/entities/Product.java
@@ -1,5 +1,6 @@
 package com.gabriel.webService.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -27,6 +28,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -81,6 +85,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> orders = new HashSet<>();
+        for (OrderItem oi : items) {
+            orders.add(oi.getOrder());
+        }
+        return orders;
     }
 
     @Override
